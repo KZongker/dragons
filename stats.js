f74c7025845688e02bb2a6abd32bde06cae6acfa
kzongker <katrina@zongker.net>
2024-06-06 17:09:36 -0500

    alertTuneUp

--- stats.js
+++ stats.js
@@ -143,7 +143,7 @@ function pullCombat(human1Index, human2Index) {
 function doBattle() {
     console.log('Fighting Vikings!!')
     if(battlerOne.value == battlerTwo.value) {
-        alert('Viking cannot fight themself!');
+        fightAlert.style.display = "block";
     } else {
        pullCombat(battlerOne.value, battlerTwo.value);
     }
@@ -168,16 +168,18 @@ const deleteBox = document.getElementById('deleteBox');
 const removeHumanBtn = document.getElementById('removeHumanBtn');
 
 const alertBox = document.getElementById('alertBox');
+const fightAlert = document.getElementById('fightAlert');
 
 function hideBox() {
     alertBox.style.display = "none";
+    fightAlert.style.display = "none";
 }
 
 function requireHuman() {
     if(vikingName.value === "") {
         alertBox.style.display = "block";
         let result = [];
-        let alteredAlert = '<div class="alert alert-warning alert-dismissible fade show text-center" role="alert">'
+        let alteredAlert = '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">'
                + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">'
                + '<path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>'
                + '</svg>'
