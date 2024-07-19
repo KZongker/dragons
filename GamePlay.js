10d00b4986dc37610ab60cd08a696cbd938e5481
kzongker <katrina@zongker.net>
2024-07-19 17:13:52 -0500

    dragonMovement

--- GamePlay.js
+++ GamePlay.js
@@ -1,3 +1,66 @@
 const gameBox = document.getElementById('gameBox');
 const player = document.getElementById('player');
 const ground = document.getElementById('ground');
+
+//temp in case player offscreen
+const resetBtn = document.getElementById('resetBtn');
+
+resetBtn.addEventListener("click", resetPosition);
+
+function resetPosition() {
+    player.style.top = 0;
+    player.style.left = 0;
+    console.log("reset");
+    return;
+}
+// code above temporary until collisions added or game finished
+
+// Start Movement
+let horizontal = 0;
+let vertical = 0;
+let speed = 9;
+/* let playerX = 0;
+let playerY = 0; */
+
+const toNum = (pxVal) => {
+    return parseInt(pxVal, 10);
+};
+
+const movement = (e) => {
+    horizontal = 0;
+    vertical = 0;
+    let leftpos = parseInt(window.getComputedStyle(player).getPropertyValue("left"))
+    let toppos = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+    switch(e.key) {
+        case 'w':
+            vertical -= 1;
+            console.log("W Key!");
+            player.style.top = toppos + vertical - speed + 'px';
+            break;
+        case 'a':
+            horizontal -= 1;
+            console.log("A Key!");
+            player.style.left = leftpos + horizontal - speed + 'px';
+            if(player.style.transform !== 'scaleX(-1)') {
+                player.style.transform = 'scaleX(-1)'; }
+            break;
+        case 's':
+            vertical += 1;
+            console.log("S Key!");
+            player.style.top = toppos + vertical + speed + 'px';
+            break;
+        case 'd':
+            horizontal += 1;
+            console.log("D Key!");
+            player.style.left = leftpos + horizontal + speed + 'px';
+            if(player.style.transform !== 'scaleX(1)') {
+            player.style.transform = 'scaleX(1)'; }
+            break;
+    }
+    /* playerX = playerX + horizontal;
+    playerY = playerY + vertical;
+    console.log(playerX, playerY); */
+};
+
+window.addEventListener("keydown", movement)
+// End Movement
