50f90005d1204621d3277e7df493c5ae56ee43b6
kzongker <katrina@zongker.net>
2024-07-20 13:37:06 -0500

    movementUpdate

--- GamePlay.js
+++ GamePlay.js
@@ -10,6 +10,8 @@ resetBtn.addEventListener("click", resetPosition);
 function resetPosition() {
     player.style.top = 0;
     player.style.left = 0;
+    if(player.style.transform !== 'scaleX(1)') {
+        player.style.transform = 'scaleX(1)'; }
     console.log("reset");
     return;
 }
@@ -19,13 +21,65 @@ function resetPosition() {
 let horizontal = 0;
 let vertical = 0;
 let speed = 9;
-/* let playerX = 0;
-let playerY = 0; */
+let playerX = 0;
+let playerY = 0;
 
 const toNum = (pxVal) => {
     return parseInt(pxVal, 10);
 };
 
+
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+    horizontal = 0;
+    vertical = 0;
+    let leftpos = parseInt(window.getComputedStyle(player).getPropertyValue("left"))
+    let toppos = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+
+    if (e.keyCode == '38' || e.keyCode == '87') {
+        if (e.keyCode == '38') {
+            event.preventDefault();
+        }
+        vertical -= 1;
+        // console.log("W Key!");
+        player.style.top = toppos + vertical - speed + 'px';
+    }
+    else if (e.keyCode == '40' || e.keyCode == '83') {
+        if (e.keyCode == '40') {
+            event.preventDefault();
+        }
+        vertical += 1;
+        // console.log("S Key!");
+        player.style.top = toppos + vertical + speed + 'px';
+    }
+    else if (e.keyCode == '37' || e.keyCode == '65') {
+        horizontal -= 1;
+        // console.log("A Key!");
+        player.style.left = leftpos + horizontal - speed + 'px';
+        if(player.style.transform !== 'scaleX(-1)') {
+            player.style.transform = 'scaleX(-1)'; }
+    }
+    else if (e.keyCode == '39' || e.keyCode == '68') {
+        horizontal += 1;
+        // console.log("D Key!");
+        player.style.left = leftpos + horizontal + speed + 'px';
+        if(player.style.transform !== 'scaleX(1)') {
+        player.style.transform = 'scaleX(1)'; }
+    }
+    playerX = playerX + horizontal;
+    playerY = playerY + vertical;
+    // console.log(playerX, playerY);
+
+}
+// End Movement
+
+
+
+/* OLD MOVEMENT
 const movement = (e) => {
     horizontal = 0;
     vertical = 0;
@@ -56,11 +110,11 @@ const movement = (e) => {
             if(player.style.transform !== 'scaleX(1)') {
             player.style.transform = 'scaleX(1)'; }
             break;
-    }
+    } */
     /* playerX = playerX + horizontal;
     playerY = playerY + vertical;
     console.log(playerX, playerY); */
-};
+// };
 
-window.addEventListener("keydown", movement)
-// End Movement
+// window.addEventListener("keydown", movement)
+//END OLD MOVEMENT
